4bb56517f5e268a45b6ddcafb0f273eee9b81052
itsumoreddy <nrk4@illinois.edu>
2020-05-06 12:56:40 +0900

    removed comment

--- app/src/main/java/com/example/calculator/HomeChecker.java
+++ app/src/main/java/com/example/calculator/HomeChecker.java
@@ -46,7 +46,6 @@ public class HomeChecker {
             if (action != null && action.equals(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)) {
                 String reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY);
                 if (reason != null) {
-                    //Log.e(TAG, "action:" + action + ",reason:" + reason);
                     if (mListener != null) {
                         if (reason.equals(SYSTEM_DIALOG_REASON_HOME_KEY)) {
                             mListener.onHomePressed();
